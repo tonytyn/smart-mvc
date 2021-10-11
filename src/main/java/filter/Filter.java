9722468dc21819e393de01b138351211e0b738cb
童艳男 <gw00179228@corp.gwm.com>
2021-10-11 10:25:18 +0800

    代码格式化ctrl+alt+l

--- src/main/java/filter/Filter.java
+++ src/main/java/filter/Filter.java
@@ -16,40 +16,40 @@ import javax.servlet.http.HttpSession;
 public class Filter implements javax.servlet.Filter {
 
     /**
-     * Default constructor. 
+     * Default constructor.
      */
     public Filter() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest)request;
-		HttpServletResponse resp = (HttpServletResponse)response;
-		HttpSession session = req.getSession();
-		if(session==null||session.getAttribute("user")==null) {
-			resp.sendRedirect(req.getContextPath()+"/login.jsp");
-		}else {
-			chain.doFilter(request, response);
-			
-		}
-		
-	}
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        if (session == null || session.getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+        } else {
+            chain.doFilter(request, response);
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
+        }
+
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+    }
 
 }
